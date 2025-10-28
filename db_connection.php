2d20daccc7104cce8919ff3e2b95a2a09c1219ab
jazb0816 <gbonillag@unah.hn>
2025-10-28 02:32:07 -0600

    Modificacion Base

--- db_connection.php
+++ db_connection.php
@@ -5,7 +5,7 @@
 $DB_HOST = 'localhost';
 $DB_USER = 'root';
 $DB_PASS = '';
-$DB_NAME = 'clinica_nutricional';
+$DB_NAME = 'clinica1';
 
 $conexion = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
